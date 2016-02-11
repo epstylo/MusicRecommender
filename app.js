979f3230c1d02c9bcb2d4628794f7348750feddd
Junoh <j.lee1103@hotmail.com>
2016-02-11 14:20:29 -0800

    Setup practice mongodb, nodejs, express project - Ref: http://www.codemag.com/article/1210041

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var http = require('http');
+var mongoose = require('mongoose');
 
+// Routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var workouts = require('./routes/workouts');
 
+// Connect to the local MongoDB instance
+// Note: if the db has never been created, it will be created upon this call.
+mongoose.connect('mongodb://localhost/workout_tracker');
 var app = express();
 
 /**
@@ -26,8 +33,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.get('/', routes);
+app.get('/users', users);
+app.get('/workouts', workouts.index);
+app.get('/workouts/:id', workouts.show);
+app.del('/workouts', workouts.delete);
+app.post('/workouts', workouts.create);
+app.put('/workouts', workouts.update);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
